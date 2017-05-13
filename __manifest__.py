1ceb0a820c97d8e370bc6e9eb0b6984715a11541
walidahmedelsayed <walidahmedelsayed1@gmail.com>
2017-05-13 18:20:15 +0200

    Adding Product Model

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Project Products',
+    'version': '1.5',
+    'author':'Walid',
+    'data':[
+        'views/products.xml'
+    ]
+}
---
+++ models/product.py
@@ -0,0 +1,21 @@
+from odoo import models , fields
+from lxml import html
+import requests
+
+class Product(models.Model):
+
+    _name = 'project.products'
+
+    name = fields.Char(string='Product')
+    category = fields.Char(string='Category')
+    price = fields.Float(string='Price')
+    image = fields.Binary()
+
+
+
+    # page = requests.get('https://deals.souq.com/eg-en?a_source=google&a_medium=cpc&a_content=search_brand&a_term=souq&u_type=text&u_mt=e&u_title=A_Souq_EX&a_campaign=SN-EG-EN-Brand&gclid=CJizqJTF49MCFQWVGwodTvUB0Q')
+    # tree = html.fromstring(page.content)
+    # details = tree.xpath('//div/img/@src')
+    #
+    #
+    # print (details)
